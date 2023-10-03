f79e309c3aa75fa118b942329dc3c8e6f0690060
Thomas Fjordside <tfjordside@skybrud.dk>
2023-10-03 10:22:50 +0200

    feat: add safeOnMounted composable

---
+++ composables/useSafeOnMounted.js
@@ -0,0 +1,26 @@
+import { onMounted } from "vue";
+
+// workaround for https://github.com/vuejs/core/issues/5844
+export function useSafeOnMounted(
+    element,
+    listener = () => {},
+    checkTimeLimit = 500
+) {
+    if (process.client) {
+        const checkInterval = 100;
+        let checksLeft = checkTimeLimit / checkInterval;
+
+        const check = () => {
+            if (element.value?.isConnected) {
+                listener();
+            } else if (checksLeft > 0) {
+                setTimeout(check, checkInterval);
+                checksLeft--;
+            }
+        };
+
+        onMounted(() => {
+            check();
+        });
+    }
+}
